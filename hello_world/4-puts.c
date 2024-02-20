032469de16117453048ac6aad52e4d12e0f12e71
fey24 <8914@holbertonstudents.com>
2024-02-20 10:51:48 +0100

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /***
  * main - program that prints exactly
- * return : Always
+ * Return: Always 0
  */
+
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
     return(0);
 }
+
