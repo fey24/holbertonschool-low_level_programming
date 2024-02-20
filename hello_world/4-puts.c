4f268e803927d78315362d935f027dc4f3ab04c0
fey24 <8914@holbertonstudents.com>
2024-02-20 10:56:17 +0100

    Fixe betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/***
+
+/**
  * main - program that prints exactly
+ *
  * Return: Always 0
  */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
